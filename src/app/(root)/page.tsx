c3cfb390cf73a081a88edf399ef379d457061121
Soumik Majumder <soumikcode02@gmail.com>
2024-12-12 13:11:47 +0530

    search startups with queries

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,15 @@
 import SearchForm from "@/components/SearchForm"
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard"
 import { STARTUP_QUERY } from "@/sanity/lib/queries"
-import { client } from "@/sanity/lib/client"
 import { sanityFetch, SanityLive } from "@/sanity/lib/live"
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  // const posts = await client.fetch(STARTUP_QUERY);
-  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY });
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params });
 
   return (
     <>
